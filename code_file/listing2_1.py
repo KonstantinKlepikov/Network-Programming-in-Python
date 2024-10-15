0056fb979147f60db2a70505d5836aa7db020b5c
Konstantin Klepikov <oformleno@gmail.com>
2024-10-16 01:42:59 +0300

    learn

--- code_file/listing2_1.py
+++ code_file/listing2_1.py
@@ -1,7 +1,9 @@
-import argparse, socket
+import argparse
+import socket
 from datetime import datetime
 
 MAX_BYTES = 65535
+
 
 def server(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,6 +17,7 @@ def server(port):
         data = text.encode('ascii')
         sock.sendto(data, address)
 
+
 def client(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     text = 'The time is {}'.format(datetime.now())
@@ -25,12 +28,20 @@ def client(port):
     text = data.decode('ascii')
     print('The server {} replied {!r}'.format(address, text))
 
+
 if __name__ == '__main__':
     choices = {'client': client, 'server': server}
-    parser = argparse.ArgumentParser(description='Send and receive UDP locally')
+    parser = argparse.ArgumentParser(
+        description='Send and receive UDP locally'
+    )
     parser.add_argument('role', choices=choices, help='which role to play')
-    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
-                        help='UDP port (default 1060)')
+    parser.add_argument(
+        '-p',
+        metavar='PORT',
+        type=int,
+        default=1060,
+        help='UDP port (default 1060)',
+    )
     args = parser.parse_args()
     function = choices[args.role]
     function(args.p)
