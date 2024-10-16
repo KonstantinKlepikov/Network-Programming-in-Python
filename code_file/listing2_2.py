af06c49002457d24e5b87b07b96e28893a26c66a
Konstantin Klepikov <oformleno@gmail.com>
2024-10-17 00:16:33 +0300

    learn

--- code_file/listing2_2.py
+++ code_file/listing2_2.py
@@ -1,6 +1,9 @@
-import argparse, random, socket, sys
+import argparse
+import random
+import socket
 
 MAX_BYTES = 65535
+
 
 def server(interface, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,13 +11,14 @@ def server(interface, port):
     print('Listening at', sock.getsockname())
     while True:
         data, address = sock.recvfrom(MAX_BYTES)
-        if random.random() < 0.5:
+        if random.random() < 0.8:
             print('Pretending to drop packet from {}'.format(address))
             continue
         text = data.decode('ascii')
         print('The client at {} says {!r}'.format(address, text))
         message = 'Your data was {} bytes long'.format(len(data))
         sock.sendto(message.encode('ascii'), address)
+
 
 def client(hostname, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -39,15 +43,25 @@ def client(hostname, port):
 
     print('The server says {!r}'.format(data.decode('ascii')))
 
+
 if __name__ == '__main__':
     choices = {'client': client, 'server': server}
-    parser = argparse.ArgumentParser(description='Send and receive UDP,'
-                                     ' pretending packets are often dropped')
+    parser = argparse.ArgumentParser(
+        description='Send and receive UDP,'
+        ' pretending packets are often dropped'
+    )
     parser.add_argument('role', choices=choices, help='which role to take')
-    parser.add_argument('host', help='interface the server listens at;'
-                        ' host the client sends to')
-    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
-                        help='UDP port (default 1060)')
+    parser.add_argument(
+        'host',
+        help='interface the server listens at;' ' host the client sends to',
+    )
+    parser.add_argument(
+        '-p',
+        metavar='PORT',
+        type=int,
+        default=1060,
+        help='UDP port (default 1060)',
+    )
     args = parser.parse_args()
     function = choices[args.role]
     function(args.host, args.p)
