f07adeac9488b64235c4a1c987520c1c8a1bcc61
Konstantin Klepikov <oformleno@gmail.com>
2024-10-17 14:21:12 +0300

    learn

--- code_file/listing2_3.py
+++ code_file/listing2_3.py
@@ -1,16 +1,20 @@
-import argparse, socket, sys
+import argparse
+import socket
+import sys
 
-# Inlined constants, because Python 3.6 has dropped the IN module.
 
 class IN:
     IP_MTU = 14
     IP_MTU_DISCOVER = 10
     IP_PMTUDISC_DO = 2
 
+
 if sys.platform != 'linux':
-    print('Unsupported: Can only perform MTU discovery on Linux',
-          file=sys.stderr)
+    print(
+        'Unsupported: Can only perform MTU discovery on Linux', file=sys.stderr
+    )
     sys.exit(1)
+
 
 def send_big_datagram(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,10 +29,16 @@ def send_big_datagram(host, port):
     else:
         print('The big datagram was sent!')
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send UDP packet to get MTU')
     parser.add_argument('host', help='the host to which to target the packet')
-    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
-                        help='UDP port (default 1060)')
+    parser.add_argument(
+        '-p',
+        metavar='PORT',
+        type=int,
+        default=1060,
+        help='UDP port (default 1060)',
+    )
     args = parser.parse_args()
     send_big_datagram(args.host, args.p)
